c1cf6e13f474f370431749de5bf6ffe6f549bad5
Joseph D <josephdykstra@gmail.com>
2014-02-20 18:52:48 -0600

    Refactorized Excercise 6.20

--- Exercise6_20.cpp
+++ Exercise6_20.cpp
@@ -1,9 +1,12 @@
 //Exercise 6.20
-int z=false;
-int multiple(int x,int y);
-int main() {z=multiple(6,4);return z;}
+
 int multiple(int x, int y)
-  {
-  if (x%y==0) {return true;}
-  else {return false;}
-  }
+	{
+	return (x%y==0)
+	}
+
+int main()
+	{
+	return multiple(6,4);
+	}
+
