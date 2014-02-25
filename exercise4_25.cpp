410c37abda18a4b321305ff3a0eda2f50f97e256
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-25 14:37:53 -0600

    Updated exercise4_25.cpp

--- exercise4_25.cpp
+++ exercise4_25.cpp
@@ -2,34 +2,41 @@
 #include <iostream>
 using namespace std;
 int a,x,y;
-void bar();
-int main()
-  {
-  cout << "Enter square's side length: ";
-  cin >> a;
-  cout << endl;
-  //a=a-1;
-  bar();
-  if (a>=2)
-    {
-    for (y=2; y<a; y++)
-      {
-      for (x=0; x<a; x++)
-        {
-        if (x==0 or x==(a-1))
-          cout << "*";
-        else
-          cout << " ";
-        }
-      cout << endl;
-      }
-    bar();
-    }
-  }
 
-void bar()
-  {
-  for (x=0; x<a; x++)
-    {cout << "*";}
-  cout << endl;
-  }
+void horizontalLine()
+	{
+	for (x=0; x<a; x++)
+		cout << "*";
+	cout << endl;
+	}
+
+void sideLines()
+	{
+	for (y=2; y<a; y++)
+		{
+		for (x=0; x<a; x++)
+			{
+			if (x==0 or x==a-1)
+				cout << "*";
+			else
+				cout << " ";
+			}
+		cout << endl;
+		}
+	}
+
+int main()
+	{
+	cout << "Enter square's side length: ";
+	cin >> a;
+	cout << endl;
+	//a=a-1;
+	horizontalLine();
+	if (a>=2)
+		{
+		sideLines();
+		horizontalLine();
+		}
+	}
+
+
